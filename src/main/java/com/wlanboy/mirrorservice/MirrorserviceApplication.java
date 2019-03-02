62b86004692f030980200bd5a51243d541419885
wlanboy <me@wlanboy.com>
2019-03-02 14:24:44 +0100

    Added Application.java and context test.

---
+++ src/main/java/com/wlanboy/mirrorservice/MirrorserviceApplication.java
@@ -0,0 +1,23 @@
+package com.wlanboy.mirrorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@SpringBootApplication
+public class MirrorserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirrorserviceApplication.class, args);
+	}
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludeHeaders(true);
+        return loggingFilter;
+    }	
+}
