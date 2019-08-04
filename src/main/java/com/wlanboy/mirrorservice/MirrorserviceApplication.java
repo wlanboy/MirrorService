f2eb8eaed86fef40ed41a2e7597be3e749b2c359
wlanboy <me@wlanboy.com>
2019-08-04 10:40:06 +0200

    Get rid of eureka, use DNS and removed Lombok.

--- src/main/java/com/wlanboy/mirrorservice/MirrorserviceApplication.java
+++ src/main/java/com/wlanboy/mirrorservice/MirrorserviceApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class MirrorserviceApplication {
 
 	public static void main(String[] args) {
