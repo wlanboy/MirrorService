d2eb671184ac9721d450947749f10634f2f26487
wlanboy <wlanboy@github.com>
2026-01-17 21:25:39 +0100

    Update to Java 25

--- src/test/java/com/wlanboy/mirrorservice/controller/MirrorControllerNegTest.java
+++ src/test/java/com/wlanboy/mirrorservice/controller/MirrorControllerNegTest.java
@@ -3,7 +3,7 @@ package com.wlanboy.mirrorservice.controller;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
