082511b3f3d5ddd42ec10e9f7fc56eed0856523f
wlanboy <wlanboy@github.com>
2026-01-18 10:36:23 +0100

    added swagger doc

--- src/main/java/com/wlanboy/mirrorservice/controller/HelloController.java
+++ src/main/java/com/wlanboy/mirrorservice/controller/HelloController.java
@@ -1,20 +1,32 @@
 package com.wlanboy.mirrorservice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HelloController {
-    @Autowired
-    private BuildProperties buildProperties;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Controller
+@Tag(name = "Home", description = "Startseiten-Endpunkt")
+public class HelloController {
+
+    private final Optional<BuildProperties> buildProperties;
+
+    public HelloController(Optional<BuildProperties> buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
+    @Operation(summary = "Startseite", description = "Zeigt die Startseite mit Service-Informationen an.")
+    @ApiResponse(responseCode = "200", description = "Startseite erfolgreich geladen")
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("serviceName", buildProperties.getName());
-        model.addAttribute("serviceVersion", buildProperties.getVersion());
+        model.addAttribute("serviceName", buildProperties.map(BuildProperties::getName).orElse("MirrorService"));
+        model.addAttribute("serviceVersion", buildProperties.map(BuildProperties::getVersion).orElse("unknown"));
         return "index";
-    }  
+    }
 }
